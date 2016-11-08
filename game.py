6b0808e63f62acb95273a4aa072f493ce18799db
sirrah23 <hseth93@gmail.com>
2016-11-08 00:17:03 -0500

    Packaged up game logic into an object

--- game.py
+++ game.py
@@ -1,58 +1,67 @@
 import pygame
 from tetpy import *
 
-BLOCK_WIDTH, BLOCK_HEIGHT = 40, 40
-GAME_WIDTH, GAME_HEIGHT = 14, 20
-TG = None
+class GameLogic(object):
+    def __init__(self, screen, BLOCK_WIDTH, BLOCK_HEIGHT, GAME_WIDTH, GAME_HEIGHT):
+        self.screen = screen
+        self.GAME_WIDTH = GAME_WIDTH
+        self.GAME_HEIGHT = GAME_HEIGHT
+        self.BLOCK_WIDTH = BLOCK_WIDTH
+        self.BLOCK_HEIGHT = BLOCK_HEIGHT
+        self.TG = TetrisGame(GAME_WIDTH, GAME_HEIGHT)
+        self.BLUE = (0, 128, 255)
+        self.PURPLE = (128, 0, 255)
 
-pygame.init()
-screen = pygame.display.set_mode((BLOCK_WIDTH*GAME_WIDTH, BLOCK_HEIGHT*GAME_HEIGHT))
-done = False
+    def game_render(self):
+        self.TG.run_iteration()
+        for y in range(self.GAME_HEIGHT):
+            for x in range(self.GAME_WIDTH):
+                #TODO: Define colors like RED and BLUE
+                if self.TG.board[y][x] == 1:  # Active piece
+                    pygame.draw.rect(self.screen, self.BLUE, pygame.Rect(x*self.BLOCK_WIDTH, y*self.BLOCK_HEIGHT, self.BLOCK_WIDTH, self.BLOCK_HEIGHT))  # Rect arguments - x, y, width, height
+                if self.TG.board[y][x] == 2:  # Inactive pieces
+                    pygame.draw.rect(self.screen, self.PURPLE, pygame.Rect(x*self.BLOCK_WIDTH, y*self.BLOCK_HEIGHT, self.BLOCK_WIDTH, self.BLOCK_HEIGHT))  # Rect arguments - x, y, width, height
 
-def game_initialize():
-    global TG
-    TG = TetrisGame(GAME_WIDTH, GAME_HEIGHT)
-    TG.run_iteration()
+    def game_process_key(self,key):
+        if key == "UP":
+            self.TG.moves += ["ROTATE"]
+        elif key == "LEFT":
+            self.TG.moves += ["LEFT"]
+        elif key == "RIGHT":
+            self.TG.moves += ["RIGHT"]
 
-def game_render():
-    TG.run_iteration()
-    for y in range(GAME_HEIGHT):
-        for x in range(GAME_WIDTH):
-            #TODO: Define colors like RED and BLUE
-            if TG.board[y][x] == 1:  # Active piece
-                pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x*BLOCK_WIDTH, y*BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT))  # Rect arguments - x, y, width, height
-            if TG.board[y][x] == 2:  # Inactive pieces
-                pygame.draw.rect(screen, (128, 0, 255), pygame.Rect(x*BLOCK_WIDTH, y*BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT))  # Rect arguments - x, y, width, height
+    def game_over(self):
+        return self.TG.gameover
 
-def game_process_key(TG, key):
-    if key == "UP":
-        TG.moves += ["ROTATE"]
-    elif key == "LEFT":
-        TG.moves += ["LEFT"]
-    elif key == "RIGHT":
-        TG.moves += ["RIGHT"]
+def main():
+    BLOCK_WIDTH, BLOCK_HEIGHT = 40, 40
+    GAME_WIDTH, GAME_HEIGHT = 14, 20
 
-def game_over(TG):
-    return TG.gameover
+    pygame.init()
+    screen = pygame.display.set_mode((BLOCK_WIDTH*GAME_WIDTH, BLOCK_HEIGHT*GAME_HEIGHT))
+    done = False
 
-game_initialize()
-while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        game_process_key(TG,"UP")
-                    if event.key == pygame.K_DOWN:
-                        game_process_key(TG,"DOWN")
-                    if event.key == pygame.K_RIGHT:
-                        game_process_key(TG,"LEFT")
-                    if event.key == pygame.K_LEFT:
-                        game_process_key(TG,"RIGHT")
-        screen.fill((0,0,0)) # Black
-        game_render()
-        if game_over(TG):
-            done = True
-        pygame.display.flip()
+    game_logic = GameLogic(screen, BLOCK_WIDTH, BLOCK_HEIGHT, GAME_WIDTH, GAME_HEIGHT)
+    while not done:
+            for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                            done = True
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP:
+                            game_logic.game_process_key("UP")
+                        if event.key == pygame.K_DOWN:
+                            game_logic.game_process_key("DOWN")
+                        if event.key == pygame.K_RIGHT:
+                            game_logic.game_process_key("LEFT")
+                        if event.key == pygame.K_LEFT:
+                            game_logic.game_process_key("RIGHT")
+            screen.fill((0,0,0)) # Black
+            game_logic.game_render()
+            if game_logic.game_over():
+                done = True
+            pygame.display.flip()
 
-# TODO: Display a GAME OVER screen?
+    # TODO: Display a GAME OVER screen?
+
+if __name__ == "__main__":
+    main()
